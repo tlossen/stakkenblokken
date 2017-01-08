65fd4bed2581e1f1272d7b01d95411d4b5617549
Tim Lossen <tim@lossen.de>
2017-01-08 19:37:21 +0100

    refactoring

--- src/main/java/stakkenblokken/Crypto.java
+++ src/main/java/stakkenblokken/Crypto.java
@@ -15,32 +15,31 @@ public class Crypto {
 
     private static final String CIPHER_AES = "AES/ECB/PKCS5Padding";
 
-    static byte[] encrypt(SecretKey key, String text) throws BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, BadPaddingException, UnsupportedEncodingException {
+        SecretKey key = generateKey();
+        printHex("key", key.getEncoded());
+        byte[] encrypted = encrypt(key, "ene mene miste, überflüssige umlaute kønnen alles kaputt machen");
+        printHex("encrypted", encrypted);
+        String decrypted = decrypt(key, encrypted);
+        System.out.println("decrypted: (" + decrypted.length() + ") " + decrypted);
+    }
+
+    private static SecretKey generateKey() throws NoSuchAlgorithmException {
+        KeyGenerator keygen = KeyGenerator.getInstance("AES");
+        keygen.init(128);
+        return keygen.generateKey();
+    }
+
+    private static byte[] encrypt(SecretKey key, String text) throws BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
         Cipher cipher = Cipher.getInstance(CIPHER_AES);
         cipher.init(Cipher.ENCRYPT_MODE, key);
         return cipher.doFinal(text.getBytes("UTF-8"));
     }
 
-    static String decrypt(SecretKey key, byte[] ct) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException {
+    private static String decrypt(SecretKey key, byte[] bytes) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException {
         Cipher cipher = Cipher.getInstance(CIPHER_AES);
         cipher.init(Cipher.DECRYPT_MODE, key);
-        byte[] pt = cipher.doFinal(ct);
-        return new String(pt, "UTF-8");
-    }
-
-    static SecretKey generateKey() throws NoSuchAlgorithmException {
-        KeyGenerator keygen = KeyGenerator.getInstance("AES");
-        keygen.init(128); // options: 128, 192, 256
-        return keygen.generateKey();
-    }
-
-    public static void main(String[] args) throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, BadPaddingException, UnsupportedEncodingException {
-        SecretKey key = generateKey();
-        printHex("key", key.getEncoded());
-        byte[] encrypted = encrypt(key, "überflüssige umlaute kønnen alles kaputt machen");
-        printHex("encrypted", encrypted);
-        String decrypted = decrypt(key, encrypted);
-        System.out.println("decrypted: " + decrypted);
+        return new String(cipher.doFinal(bytes), "UTF-8");
     }
 
     private static void printHex(String label, byte[] bytes) {
@@ -48,14 +47,13 @@ public class Crypto {
         System.out.println(label + ": (" + bytes.length + ") " + hex);
     }
 
-    private static final String HEX = "0123456789abcdef";
-
     private static String toHex(byte[] bytes) {
-        final StringBuilder hex = new StringBuilder(2 * bytes.length);
+        final String HEX = "0123456789abcdef";
+        final StringBuilder result = new StringBuilder(2 * bytes.length);
         for (final byte b : bytes) {
-            hex.append(HEX.charAt((b & 0xF0) >> 4)).append(HEX.charAt((b & 0x0F)));
+            result.append(HEX.charAt((b & 0xF0) >> 4)).append(HEX.charAt((b & 0x0F)));
         }
-        return hex.toString();
+        return result.toString();
     }
 
 }
